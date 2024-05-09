3748d11a806db6eb10ae0e79f2038c5d0026719d
orchardyst <115893088+orchardyst@users.noreply.github.com>
2024-05-09 22:32:11 +0300

    Update jenkins.tf

--- JENKINS/jenkins.tf
+++ JENKINS/jenkins.tf
@@ -1,134 +1,133 @@
-# configured aws provider with proper credentials
+# Configure AWS provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "yusuf"
+  region = "us-east-1"
 }
-
-
-# create default vpc if one does not exit
+ 
+# Create default VPC if one does not exist
 resource "aws_default_vpc" "default_vpc" {
-
-  tags    = {
-    Name  = "default vpc"
+  tags = {
+    Name = "default vpc"
   }
 }
 
-
-# use data source to get all avalablility zones in region
+# Use data source to get all availability zones in region
 data "aws_availability_zones" "available_zones" {}
 
-
-# create default subnet if one does not exit
+# Create default subnet if one does not exist
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.available_zones.names[0]
 
-  tags   = {
+  tags = {
     Name = "default subnet"
   }
 }
 
-
-# create security group for the ec2 instance
+# Create security group for the EC2 instance
 resource "aws_security_group" "ec2_security_group_jenkins" {
   name        = "ec2 security group_jenkins"
-  description = "allow access on ports 8080 and 22"
+  description = "allow access on ports 8080, 22, and 9000"
   vpc_id      = aws_default_vpc.default_vpc.id
 
-  # allow access on port 8080
+  # Allow access on port 8080 (HTTP)
   ingress {
-    description      = "http proxy access"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP proxy access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # allow access on port 22
+  # Allow access on port 22 (SSH)
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow access on port 9000 (SonarQube)
+  ingress {
+    description = "SonarQube access"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all egress traffic
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
  
-  tags   = {
+  tags = {
     Name = "jenkins server security group"
   }
 }
 
-
-# use data source to get a registered amazon linux 2 ami
+# Use data source to get a registered Amazon Linux 2 AMI
 data "aws_ami" "ubuntu" {
+  most_recent = true
 
-    most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    owners = ["099720109477"]
+  owners = ["099720109477"]
 }
 
-# launch the ec2 instance and install website
+# Launch the EC2 instance and install website
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group_jenkins.id]
-  key_name               = "devopskeypair"
-  # user_data            = file("install_jenkins.sh")
+  key_name               = "Udemyclass2"
+  user_data            = file("install_jenkins.sh")
 
   tags = {
     Name = "jenkins_server"
   }
 }
 
-
-# an empty resource block
+/*# An empty resource block
 resource "null_resource" "name" {
-
-  # ssh into the ec2 instance 
+  # SSH into the EC2 instance 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/Downloads/devopskeypair.pem")
+    private_key = file("~/Downloads/Udemyclass2.pem")
     host        = aws_instance.ec2_instance.public_ip
   }
 
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
+  # Copy the install_jenkins.sh file from your computer to the EC2 instance 
   provisioner "file" {
     source      = "install_jenkins.sh"
     destination = "/tmp/install_jenkins.sh"
   }
 
-  # set permissions and run the install_jenkins.sh file
+  # Set permissions and run the install_jenkins.sh file
   provisioner "remote-exec" {
     inline = [
-        "sudo chmod +x /tmp/install_jenkins.sh",
-        "sh /tmp/install_jenkins.sh",
+      "sudo chmod +x /tmp/install_jenkins.sh",
+      "sh /tmp/install_jenkins.sh",
     ]
   }
 
-  # wait for ec2 to be created
+  # Wait for EC2 to be created
   depends_on = [aws_instance.ec2_instance]
-}
+} */
 
-
-# print the url of the jenkins server
+# Print the URL of the Jenkins server
 output "website_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+  value = "http://${aws_instance.ec2_instance.public_dns}:8080"
 }
